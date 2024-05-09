70343560d488841e9e8b3c6b6727a1ea512c167a
Xatan01 <149221783+Xatan01@users.noreply.github.com>
2024-05-09 17:40:48 +0800

    added token for login

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Import `Routes` and `Navigate` from react-router-dom
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './Pages/loginPage';
+//import HomePage from './Pages/homePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Apps() {
+function App() {
+  const isAuthenticated = () => {
+    // Check if the user is authenticated (e.g., by checking if the session token or JWT exists)
+    return localStorage.getItem('token') != null;
+  };
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="login" />} /> 
-          <Route path="login" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </div>
   );
 }
 
-export default Apps;
+export default App;
